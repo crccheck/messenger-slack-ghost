5444c78e66e62717a860a099af2a5c0fa4c4c655
crccheck <c@crccheck.com>
2017-03-09 16:22:18 -0600

    switch to node-slack

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
 const { Messenger } = require('launch-vehicle-fbm')
-const reqPromise = require('request-promise')
+const { MemoryDataStore, RtmClient, WebClient } = require('@slack/client')
+const RTM_CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS.RTM
 
+const CHANNEL = process.env.SLACK_CHANNEL
+
+
+const web = new WebClient(process.env.SLACK_API_TOKEN)
 const messenger = new Messenger()
-
-messenger.on('text', ({text, session}) => {
-  return reqPromise.post({
-    url: process.env.SLACK_WEBHOOK_URL,
-    body: {
-      channel: process.env.SLACK_CHANNEL,
-      icon_url: session.profile.profile_pic,
-      username: `${session.profile.first_name} ${session.profile.last_name}`,
-      text
-    },
-    json: true
-  })
+const rtm = new RtmClient(process.env.SLACK_API_TOKEN, {
+  logLevel: 'error',
+  dataStore: new MemoryDataStore()
 })
 
+function getChannelId(dataStore, name) {
+  const needle = name.replace(/^#/, '')
+  const data = dataStore.getChannelByName(needle) || dataStore.getGroupByName(needle)
+  return data.id
+}
+
+rtm.on(RTM_CLIENT_EVENTS.RTM_CONNECTION_OPENED, () => {
+  const id = getChannelId(rtm.dataStore, CHANNEL)
+
+  messenger.on('text', ({text, session}) => {
+    console.log(session.profile)
+    // the rtm client can't override icon_url/username
+    web.chat.postMessage(id, text, {
+      icon_url: session.profile.profile_pic,
+      username: `${session.profile.first_name} ${session.profile.last_name}`,
+      thread_ts: session.thread_ts
+    }, (err, res) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+
+      if (!session.thread_ts) {
+        session.thread_ts = res.ts
+      }
+      console.log(res)
+    })
+  })
+
+  const user = rtm.dataStore.getUserById(rtm.activeUserId)
+  const team = rtm.dataStore.getTeamById(rtm.activeTeamId)
+  console.log('Connected to ' + team.name + ' as ' + user.name)
+});
+
 messenger.start()
+rtm.start()
