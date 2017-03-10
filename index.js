d129660b4aa2a1660d8228992d247a1440d99116
Chris Chang <c@crccheck.com>
2017-03-10 13:27:35 -0600

    Add ability for humans to intervene (#2)
    * refactor to make thread store a map
    
    * give feedback when talking to dead threads

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const { Messenger } = require('launch-vehicle-fbm')
+const { Messenger, Text } = require('launch-vehicle-fbm')
 const { MemoryDataStore, RtmClient, WebClient } = require('@slack/client')
 const RTM_CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS.RTM
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS
 
 const CHANNEL = process.env.SLACK_CHANNEL
 
@@ -18,7 +19,16 @@ function getChannelId(name, dataStore) {
   return data.id
 }
 
-const threadStore = {}
+const threadStore = new Map()
+
+function findSenderForThread(ts) {
+  let key, value
+  for ([key, value] of threadStore) {
+    if (value === ts) {
+      return key
+    }
+  }
+}
 
 rtm.on(RTM_CLIENT_EVENTS.RTM_CONNECTION_OPENED, () => {
   const id = getChannelId(CHANNEL, rtm.dataStore)
@@ -33,19 +43,19 @@ rtm.on(RTM_CLIENT_EVENTS.RTM_CONNECTION_OPENED, () => {
       username = `${session.profile.first_name} ${session.profile.last_name}`
       threadKey = senderId
     }
-    // Use the web client b/c the rtm client can't override icon_url/username
+    // Use the web client b/c the rtm client can't override icon_url/username or do threads
     web.chat.postMessage(id, text, {
       icon_url: session.profile.profile_pic,
       username,
-      thread_ts: threadStore[threadKey]
+      thread_ts: threadStore.get(threadKey)
     }, (err, res) => {
       if (err) {
         console.error(err)
         return
       }
 
-      if (!threadStore[threadKey]) {
-        threadStore[threadKey] = res.ts
+      if (!threadStore.has(threadKey)) {
+        threadStore.set(threadKey, res.ts)
       }
     })
   })
@@ -53,7 +63,24 @@ rtm.on(RTM_CLIENT_EVENTS.RTM_CONNECTION_OPENED, () => {
   const user = rtm.dataStore.getUserById(rtm.activeUserId)
   const team = rtm.dataStore.getTeamById(rtm.activeTeamId)
   console.log(`Connected to ${team.name} as ${user.name}`)
-});
+})
+
+rtm.on(RTM_EVENTS.MESSAGE, (message) => {
+  if (!message.thread_ts || !message.user) {
+    // must be in a thread, and must be from a human
+    return
+  }
+
+  const senderId = findSenderForThread(message.thread_ts)
+  if (senderId) {
+    messenger.send(senderId, new Text(message.text))
+  } else {
+    web.chat.postMessage(message.channel, '_Sorry, but this thread is closed to new messages_', {
+      thread_ts: message.thread_ts
+    });
+    console.error(`No thread found ${message.text}`)
+  }
+})
 
 messenger.start()
 rtm.start()
